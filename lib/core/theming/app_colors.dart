3d609c42e87d1c7fed0bfa84f0096fc364f2f1c0
Seif Elnawawy <elnawawyseif@gmail.com>
2025-11-16 11:19:20 +0200

    - Set the initial theme for the app

---
+++ lib/core/theming/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/widgets.dart';
+
+class AppColors {
+  AppColors._();
+  static const Color primaryColor = Color(0xff247CFF);
+  static const Color grey = Color(0xff757575);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xff242424);
+}
---
+++ lib/core/theming/app_text_styles.dart
@@ -0,0 +1,30 @@
+import 'package:doctor_app/core/theming/app_colors.dart';
+import 'package:flutter/widgets.dart';
+
+class AppTextStyles {
+  AppTextStyles._();
+
+  static TextStyle font24Black700 = const TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w700,
+    color: AppColors.black,
+  );
+  static TextStyle font32Blue700 = const TextStyle(
+    fontSize: 32,
+    fontWeight: FontWeight.w700,
+    color: AppColors.primaryColor,
+    height: 1.5,
+  );
+  static TextStyle font12Grey400 = const TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    color: AppColors.grey,
+    height: 1.5,
+  );
+  static TextStyle font16White600 = const TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    color: AppColors.white,
+    height: 1.5,
+  );
+}
