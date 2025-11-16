4ba8d4c288a66814a3d13d54850ef46621143164
Seif Elnawawy <elnawawyseif@gmail.com>
2025-11-16 11:19:56 +0200

    separate main file from material app and added flutter screen util

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,7 @@
+import 'package:doctor_app/core/routing/app_router.dart';
+import 'package:doctor_app/doctor_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false);
-  }
+  runApp(DoctorApp(appRouter: AppRouter()));
 }
